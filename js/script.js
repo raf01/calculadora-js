2f8f82c42595e02e050eba49d8ebb7662d2b4531
Rafael Pontes <rafaelgpontes1@gmail.com>
2016-07-09 00:07:28 -0300

    Melhoras no design e avanços no javascript.

--- js/script.js
+++ js/script.js
@@ -1,18 +1,53 @@
+/**
+  @author Rafael G. Pontes
+*/
 var buttons = Array.from(document.querySelectorAll('button'));
 buttons.forEach(function(button){
-  let operator = button.innerHTML;
-  let operand1 = document.querySelector('#operand1');
-  let operand2 = document.querySelector('#operand2');
+  let bValue = button.innerHTML;
+  let expression = document.querySelector('.expression');
   let result = document.querySelector('#result');
-
+  console.log("New forEach iteration...");
   button.onclick = function(event){
     event.preventDefault();
-    result.value = calc(operand1.value, operand2.value, operator);
+    console.log("onclick fired...");
+    expression.value = getStringExpression(expression.value, bValue);
+    result.value = calc(getMathExpression(expression.value));
   }
 });
 
-function calc(operand1, operand2, operator){
-  operator = (operator == 'ⅹ') ? '*' : operator;
-  operator = (operator == '÷') ? '/' : operator;
-  return eval(`${operand1} ${operator} ${operand2}`)
+function getMathExpression(expression){
+  var mathExpression = '';
+  console.log("getMathExpression()...")
+  console.log("expression.length = " + expression.length);
+  for(var i = 0; i < expression.length; i++){
+    switch (expression[i]) {
+      case 'ⅹ': mathExpression.concat('*'); break;
+      case '÷': mathExpression.concat('/'); break;
+      case '=':
+        i = expression.length + 1;
+        break;
+      case '⌫':
+        mathExpression = mathExpression.substring(0, mathExpression.length-1);
+        console.log("Here...")
+        break;
+      case 'Clear':
+        mathExpression = '';
+        i = expression.length + 1;
+        break;
+      default:
+        console.log("default... adding '" + expression[i] + "'")
+        mathExpression.concat(expression[i]);
+    }
+  }
+  console.log("final mathExpr = '" + mathExpression + "'")
+  return mathExpression;
+}
+
+function getStringExpression(expression, bValue) {
+    expression = expression + bValue;
+    return expression;
+}
+
+function calc(mathExpression) {
+  return eval(`${mathExpression}`)
 }
